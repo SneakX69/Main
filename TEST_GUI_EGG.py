bcb93e20669078647c3def6699816c56c86a4bf9
Johannes <johannes.spari@outlook.de>
2025-08-26 08:55:20 -1100

    Update TEST_GUI_EGG.py

--- TEST_GUI_EGG.py
+++ TEST_GUI_EGG.py
@@ -1,41 +1,40 @@
-import time
 import tkinter as tk
+
+def countdown(sekunden):
+    if sekunden >= 0:
+        Sekunden.config(text=f"Verbleibende Zeit: {sekunden} Sekunden")
+        root.after(1000, countdown, sekunden-1)
+    else:
+        Sekunden.config(text="Guten Appetit!")
 
 root = tk.Tk()
 root.title("Ei-Timer GUI")
-root.configure(bg="lightyellow")
+root.configure(bg="yellow")
 
-Head = tk.Label(root, text="Ei-Timer Version 1.7", bg="white", font="Sans-Serif 16")
-Head.pack(pady=10)
-
-label = tk.Label(root,text="Wie willst du dein EI?\nWähle aus:")
+label = tk.Label(root, text="Ei-Timer Version 1.4", bg="white", font=("Times New Roman", 16))
 label.pack(pady=10)
 
-Durchgebraten = tk.Button(root, text="Durchgebraten")
-Durchgebraten.pack(pady=10)
-
-Weich = tk.Button(root, text="Weich")
-Weich.pack(pady=10)
-
-Ultraweich =tk.Button(root, text="Ultraweich")
-Ultraweich.pack(pady=10)
+head = tk.Label(root, text="Wie willst du dein Ei?")
+head.pack(pady=10)
 
 Sekunden = tk.Label(root, text="Verbleibende Zeit:")
 Sekunden.pack(pady=10)
 
-def countdown(sekunden):
-    if sekunden >=0:
-        Sekunden.config(root, text=f"Verbleibende Zeit: {sekunden} Sekunden")
-        root.after(1000, countdown, sekunden-1)
+Durchgebraten = tk.Button(root, text="Durchgebraten", command=lambda: countdown(600)) # 10 Minuten
+Durchgebraten.pack(pady=10)
 
+Weich = tk.Button(root, text="Weich", command=lambda: countdown(360)) # 6 Minuten
+Weich.pack(pady=10)
 
-if Durchgebraten == pressed():
-    countdown(600) # 10 Minuten
-elif Weich == True:
-    countdown(360) # 6 Minuten
-elif Ultraweich == True:
-    countdown(180)
+Ultraweich = tk.Button(root, text="Ultraweich", command=lambda: countdown(180)) # 3 Minuten
+Ultraweich.pack(pady=10)
+
 root.mainloop()
+
+
+
+
+
 
 
 
